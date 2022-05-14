276c7c12c6a5c5c20e9c2c6abc51ae6712a4f630
erix_syaifulr <erix.rohman@lidahbuayagroup.com>
2022-05-14 07:45:52 +0700

    modify home page with tab bar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:erixquran/app/constant/color.dart';
 import 'package:erixquran/app/data/models/surah.dart';
 import 'package:erixquran/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
@@ -9,12 +10,97 @@ import '../controllers/home_controller.dart';
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: FutureBuilder<List<Surah>>(
+    Widget lastRead() {
+      return Container(
+        height: 150,
+        width: Get.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            appPurplueLight1,
+            appPurplueDark,
+          ]),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Material(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(20),
+            onTap: () {
+              Get.toNamed(Routes.LAST_READ);
+            },
+            child: Stack(children: [
+              Positioned(
+                bottom: -30,
+                right: 0,
+                child: Opacity(
+                  opacity: 0.6,
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    child: Image.asset(
+                      "assets/images/quran-logo.png",
+                      color: appWhite,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.menu_book_rounded, color: appWhite),
+                        SizedBox(width: 10),
+                        Text("Terakhir dibaca",
+                            style: TextStyle(color: appWhite)),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text("Al Fatihah",
+                            style: TextStyle(color: appWhite, fontSize: 20)),
+                        SizedBox(height: 5),
+                        Text("Juz 1 | Ayat 5",
+                            style: TextStyle(color: appWhite)),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+        ),
+      );
+    }
+
+    Widget tabBar() {
+      return TabBar(
+        indicator: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: appPurplueLight1,
+        ),
+        labelColor: appWhite,
+        unselectedLabelColor: appPurplue,
+        tabs: [
+          Tab(
+            child: Text("Surah"),
+          ),
+          Tab(
+            child: Text("Juz"),
+          ),
+          Tab(
+            child: Text("Bookmark"),
+          ),
+        ],
+      );
+    }
+
+    Widget surahTabBarView() {
+      return FutureBuilder<List<Surah>>(
         future: controller.getAllSurah(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -46,7 +132,56 @@ class HomeView extends GetView<HomeController> {
             },
           );
         },
-      ),
-    );
+      );
+    }
+
+    Widget tabBarView() {
+      return Expanded(
+        child: TabBarView(
+          children: [
+            surahTabBarView(),
+            Center(
+              child: Text("Juz"),
+            ),
+            Center(
+              child: Text("Bookmark"),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget body() {
+      return DefaultTabController(
+        length: 3,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Assalamu'alaikum",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 20),
+              lastRead(),
+              SizedBox(height: 20),
+              tabBar(),
+              tabBarView(),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Al-Quran App'),
+          centerTitle: true,
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          ],
+        ),
+        body: body());
   }
 }
