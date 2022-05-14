a398eac34418c5463d979f66da5fbbc25e7096eb
erix_syaifulr <erix.rohman@lidahbuayagroup.com>
2022-05-14 07:45:35 +0700

    modify introduction page

--- lib/app/modules/introduction/views/introduction_view.dart
+++ lib/app/modules/introduction/views/introduction_view.dart
@@ -1,3 +1,4 @@
+import 'package:erixquran/app/constant/color.dart';
 import 'package:erixquran/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 
@@ -22,27 +23,40 @@ class IntroductionView extends GetView<IntroductionController> {
           ),
           SizedBox(height: 20),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 50),
             child: Text(
-              'Sesibuk itukah kamu sampai belum membaca Al-Quran',
+              'Sesibuk itukah kamu sampai belum membaca Al-Quran ?',
               style: TextStyle(
                 fontSize: 16,
               ),
               textAlign: TextAlign.center,
             ),
           ),
-          SizedBox(height: 20),
-          Container(
-            width: 250,
-            height: 250,
-            child: Lottie.asset("assets/lotties/quran-animation.json"),
+          SizedBox(height: 70),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              width: 250,
+              height: 250,
+              child: Lottie.asset("assets/lotties/quran-animation.json"),
+            ),
           ),
           SizedBox(height: 20),
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Get.isDarkMode ? appWhite : appPurplue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
             onPressed: () {
               Get.offAllNamed(Routes.HOME);
             },
-            child: Text('Mulai'),
+            child: Text(
+              'GET STARTED',
+              style:
+                  TextStyle(color: Get.isDarkMode ? appPurplueDark : appWhite),
+            ),
           ),
         ],
       ),
