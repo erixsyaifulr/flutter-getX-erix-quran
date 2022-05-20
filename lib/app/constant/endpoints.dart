b16aa9c535d21a9b87858a8a4d80e23ac1e406fe
erix_syaifulr <erix.rohman@lidahbuayagroup.com>
2022-05-20 19:09:35 +0700

    add new file to save endpoint list

---
+++ lib/app/constant/endpoints.dart
@@ -0,0 +1,8 @@
+class Endpoint {
+  Endpoint._();
+
+  static const String baseUrl = "https://api.quran.sutanlab.id/";
+
+  static const String getSurahUrl = baseUrl + "surah";
+  static const String getDetailJuzUrl = baseUrl + "juz/";
+}
