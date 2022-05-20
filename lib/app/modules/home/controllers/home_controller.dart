4cd7c7f6ee2b1e6d53bd5c80fabb43fb47ad5d50
erix_syaifulr <erix.rohman@lidahbuayagroup.com>
2022-05-20 19:11:54 +0700

    add new function to get all juz data

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:erixquran/app/constant/endpoints.dart';
+import 'package:erixquran/app/data/models/juz.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
@@ -7,12 +9,26 @@ import '../../../data/models/surah.dart';
 
 class HomeController extends GetxController {
   RxBool isDark = false.obs;
+  List<Surah> allSurah = [];
   Future<List<Surah>> getAllSurah() async {
-    var res = await http.get(Uri.parse("https://api.quran.sutanlab.id/surah"));
+    var res = await http.get(Uri.parse(Endpoint.getSurahUrl));
     List data = (jsonDecode(res.body) as Map<String, dynamic>)["data"];
     if (data.isEmpty) {
       return [];
     }
-    return data.map((e) => Surah.fromJson(e)).toList();
+    allSurah = data.map((e) => Surah.fromJson(e)).toList();
+    return allSurah;
+  }
+
+  Future<List<Juz>> getAllJuz() async {
+    List<Juz> allJuz = [];
+    for (int i = 1; i <= 30; i++) {
+      var res = await http.get(Uri.parse(Endpoint.getDetailJuzUrl + "$i"));
+      Map<String, dynamic> data =
+          (jsonDecode(res.body) as Map<String, dynamic>)["data"];
+      Juz juz = Juz.fromJson(data);
+      allJuz.add(juz);
+    }
+    return allJuz;
   }
 }
