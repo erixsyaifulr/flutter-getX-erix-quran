e959e5cf0c59f5feb993d940dd8f4d02f6ea65d2
erix_syaifulr <erix.rohman@lidahbuayagroup.com>
2022-05-27 07:45:48 +0700

    integrate get_storage and change theme with local storage

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,19 @@ import 'package:erixquran/app/constant/colors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  final box = GetStorage();
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Application",
-      theme: lightTheme,
+      theme: box.read("themeDark") == null ? lightTheme : darkTheme,
       initialRoute: Routes.INTRODUCTION,
       getPages: AppPages.routes,
     ),
