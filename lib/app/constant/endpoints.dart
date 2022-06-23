4fd85e46b05993d53836c523f5fcce3c6e9f33e6
erix_syaifulr <erix.rohman@lidahbuayagroup.com>
2022-06-23 20:45:17 +0700

    change endpoint base url

--- lib/app/constant/endpoints.dart
+++ lib/app/constant/endpoints.dart
@@ -1,7 +1,7 @@
 class Endpoint {
   Endpoint._();
 
-  static const String baseUrl = "https://api.quran.sutanlab.id/";
+  static const String baseUrl = "https://erix-quran-api.vercel.app/";
 
   static const String getSurahUrl = baseUrl + "surah";
   static const String getDetailJuzUrl = baseUrl + "juz/";
